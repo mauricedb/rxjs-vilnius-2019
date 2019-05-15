ac1879fd279db19c16b1f9282b004166bbfcb874
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-05-15 22:07:26 +0300

    Ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import { fromEvent, Observable } from "rxjs";
-import { take, switchMap, map, pairwise, takeUntil } from "rxjs/operators";
+import { interval, fromEvent, Observable } from "rxjs";
+import { tap, take, switchMap, map, pairwise, takeUntil } from "rxjs/operators";
 
 const start = document.getElementById("start");
-const canvas = document.getElementById("canvas");
