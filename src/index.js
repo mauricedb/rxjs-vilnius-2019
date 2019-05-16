4a2283063673fc29efbc8ca7852a41d8d6ff26ea
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-05-16 14:06:19 +0300

    Paint demo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-import { interval, fromEvent, Observable } from "rxjs";
-import { tap, take, switchMap, map, pairwise, takeUntil } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { switchMap, map, pairwise, takeUntil } from "rxjs/operators";
 
-const start = document.getElementById("start");
+const canvas = document.getElementById("canvas");
+
+const mouseDown$ = fromEvent(canvas, "mousedown");
+const mouseMove$ = fromEvent(canvas, "mousemove");
+const mouseUp$ = fromEvent(canvas, "mouseup");
+
+mouseDown$
+  .pipe(
+    switchMap(() =>
+      mouseMove$.pipe(
+        map(e => ({
+          ctx: e.target.getContext("2d"),
+          x: e.offsetX,
+          y: e.offsetY
+        })),
+        pairwise(),
+        takeUntil(mouseUp$)
+      )
+    )
+  )
+  .subscribe(pair => {
+    const [from, to] = pair;
+    const { ctx } = from;
+
+    ctx.beginPath();
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+  });
